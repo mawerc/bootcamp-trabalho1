358b65f65475bfb2255c2b4e8db698b60e6d9d33
Mawer Cardoso <mawerc@gmail.com>
2020-10-02 14:39:12 +0100

    Refactorando Service and Resource, adding exceptions

--- backend/src/main/java/com/mawercardoso/trabalho1/resources/ClientResource.java
+++ backend/src/main/java/com/mawercardoso/trabalho1/resources/ClientResource.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -26,7 +27,7 @@ class ClientResource implements Serializable{
 	@GetMapping
 	public ResponseEntity<Page<ClientDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "linesPerPage", defaultValue = "16") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
 			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
 			){
@@ -36,5 +37,11 @@ class ClientResource implements Serializable{
 		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- backend/src/main/java/com/mawercardoso/trabalho1/services/ClientService.java
+++ backend/src/main/java/com/mawercardoso/trabalho1/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.mawercardoso.trabalho1.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.mawercardoso.trabalho1.dto.ClientDTO;
 import com.mawercardoso.trabalho1.entities.Client;
 import com.mawercardoso.trabalho1.repositories.ClientRepository;
+import com.mawercardoso.trabalho1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -22,6 +25,14 @@ public class ClientService {
 		
 		return page.map(x -> new ClientDTO(x));
 		
+	}
+	
+	@Transactional
+	public ClientDTO findById(Long id) {
+		
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entidade não encontrada"));
+		return new ClientDTO(entity);
 	}
 	
 
