41523143013ba74942db8359006858d3bb2eede3
Mawer Cardoso <mawerc@gmail.com>
2020-10-02 15:12:20 +0100

    POST Client

--- backend/src/main/java/com/mawercardoso/trabalho1/resources/ClientResource.java
+++ backend/src/main/java/com/mawercardoso/trabalho1/resources/ClientResource.java
@@ -1,6 +1,7 @@
 package com.mawercardoso.trabalho1.resources;
 
 import java.io.Serializable;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,9 +10,12 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mawercardoso.trabalho1.dto.ClientDTO;
 import com.mawercardoso.trabalho1.services.ClientService;
@@ -44,4 +48,13 @@ class ClientResource implements Serializable{
 		ClientDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
+
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id})")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
 }
--- backend/src/main/java/com/mawercardoso/trabalho1/services/ClientService.java
+++ backend/src/main/java/com/mawercardoso/trabalho1/services/ClientService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.mawercardoso.trabalho1.dto.ClientDTO;
 import com.mawercardoso.trabalho1.entities.Client;
@@ -27,12 +28,30 @@ public class ClientService {
 		
 	}
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entidade não encontrada"));
 		return new ClientDTO(entity);
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO insert(@RequestBody ClientDTO dto){
+		Client entity = copytoEntity(dto);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	private Client copytoEntity(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setId(dto.getId());
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		return entity;
 	}
 	
 
